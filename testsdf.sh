ad94bdd306ec8f82c9d64b4c2da64dbd8da6dc28
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2011-12-09 13:31:57 +0000

    removed a lot of things related to sdf

--- testsdf.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# $1 file
-# $2 .. $9 optional cmd line arguments
-
-java -Xss16m -Xmx1500m -cp bin:lib/aterm-java.jar:lib/shared-objects.jar:lib/jjtraveler.jar nl.cwi.sen1.AmbiDexter.Main $2 $3 $4 $5 $6 $7 $8 $9 $1
